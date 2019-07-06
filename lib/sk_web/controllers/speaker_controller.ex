4873ebe7daf7fe3e58daa66c9521a821137ee466
Howard M. Lewis Ship <hlship@walmartlabs.com>
2019-07-05 18:07:58 -0700

    End of chapter 3

--- lib/sk_web/controllers/speaker_controller.ex
+++ lib/sk_web/controllers/speaker_controller.ex
@@ -8,4 +8,11 @@ defmodule SkWeb.SpeakerController do
     speakers = Accounts.list_speakers()
     render(conn, "index.html", speakers: speakers)
   end
+
+  def show(conn, %{"id" => id}) do
+    speaker = Accounts.get_speaker(id)
+
+    render(conn, "view.html", speaker: speaker)
+  end
+
 end
