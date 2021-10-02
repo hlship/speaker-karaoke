0003083aa8e302675f97b68b312f877cbdc43478
Howard Lewis Ship <h@lewisship.net>
2021-10-02 10:10:19 -0700

    Setup for production asset build

--- mix.exs
+++ mix.exs
@@ -66,7 +66,10 @@ defmodule Sk.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "assets.deploy": [
+        "cmd --cd assets npm run deploy",
+        "esbuild default --minify",
+        "phx.digest"],
       "prod.server": [
         "assets.deploy",
         "phx.server"
